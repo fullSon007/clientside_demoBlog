f855398ec16b0f59478c661cbd56e36d4d89b904
Đào Thái Sơn <thaisonn0701@gmail.com>
2023-11-30 23:05:56 +0700

    trying with react-websocket

--- app/maps/page.js
+++ app/maps/page.js
@@ -8,56 +8,74 @@ import Map from "@/components/Map/Map";
 
 // const Map = dynamic(() => import('../components/Map/Map'), { ssr: false });
 
-export default function Home({ initialShips }) {
+export default function Home() {
+    // const [ships, setShips] = useState([]);
+    // const [selectedShip, setSelectedShip] = useState(null);
+    // const { auth } = useAuth()
+
+    // useEffect(() => {
+    //     // Sample data
+    //     const initialShips = [
+    //         { id: 1, name: 'Ship 1', lat: 0, lng: 0 },
+    //         { id: 2, name: 'Ship 2', lat: 10, lng: 10 },
+    //     ];
+    //     setShips(initialShips);
+    // }, []);
+
+
+    // const addShip = () => {
+    //     const shipName = prompt('Enter ship name:');
+    //     const lat = parseFloat(prompt('Enter latitude:'));
+    //     const lng = parseFloat(prompt('Enter longitude:'));
+
+    //     if (!isNaN(lat) && !isNaN(lng)) {
+    //         const newShip = { id: ships.length + 1, name: shipName, lat, lng };
+    //         setShips([...ships, newShip]);
+    //       } else {
+    //         alert('Invalid latitude or longitude. Please enter numeric values.');
+    //       }
+    //     };
+
+
+    //     const deleteShip = (shipId) => {
+    //       setShips(ships.filter((ship) => ship.id !== shipId));
+    //     };
+
+    //     const updateShip = (shipId, newShip) => {
+    //       setShips(ships.map((ship) => (ship.id === shipId ? newShip : ship)));
+    //     };
+
     const [ships, setShips] = useState([]);
-    const [selectedShip, setSelectedShip] = useState(null);
     const { auth } = useAuth()
+  const addShip = () => {
+    const shipName = prompt('Enter ship name:');
+    const lat = parseFloat(prompt('Enter latitude:'));
+    const lng = parseFloat(prompt('Enter longitude:'));
 
-    useEffect(() => {
-        // Sample data
-        const initialShips = [
-            { id: 1, name: 'Ship 1', lat: 0, lng: 0 },
-            { id: 2, name: 'Ship 2', lat: 10, lng: 10 },
-        ];
-        setShips(initialShips);
-    }, []);
+    if (!isNaN(lat) && !isNaN(lng)) {
+      const newShip = { id: ships.length + 1, name: shipName, lat, lng };
+      setShips([...ships, newShip]);
+    } else {
+      alert('Invalid latitude or longitude. Please enter numeric values.');
+    }
+  };
 
-  //   useEffect(() => {
-  //       // Function to fetch data from the API
-  //       const fetchData = async () => {
-  //         try {
-  //           const response = await axios.get('https://data.aishub.net/ws.php?username=USERNAME&format=1&output=json&compress=3&latmin=20.5&latmax=30.8&lonmin=-15&lonmax=18.6');
-  //           setShips(response.data);
-  //         } catch (error) {
-  //           console.error('Error fetching data:', error);
-  //         }
-  //       };
+  const deleteShip = (shipId) => {
+    setShips(ships.filter((ship) => ship.id !== shipId));
+  };
 
-  //       fetchData();
-  // }, []);
+  const updateShip = (shipId, newShip) => {
+    setShips(ships.map((ship) => (ship.id === shipId ? newShip : ship)));
+  };
 
-
-    const addShip = () => {
-        const shipName = prompt('Enter ship name:');
-        const lat = parseFloat(prompt('Enter latitude:'));
-        const lng = parseFloat(prompt('Enter longitude:'));
-
-        if (!isNaN(lat) && !isNaN(lng)) {
-            const newShip = { id: ships.length + 1, name: shipName, lat, lng };
-            setShips([...ships, newShip]);
-          } else {
-            alert('Invalid latitude or longitude. Please enter numeric values.');
-          }
-        };
-
-
-        const deleteShip = (shipId) => {
-          setShips(ships.filter((ship) => ship.id !== shipId));
-        };
-
-        const updateShip = (shipId, newShip) => {
-          setShips(ships.map((ship) => (ship.id === shipId ? newShip : ship)));
-        };
+  useEffect(() => {
+    // Sample data
+    const initialShips = [
+      { id: 1, name: 'Ship 1', lat: 0, lng: 0 },
+      { id: 2, name: 'Ship 2', lat: 10, lng: 10 },
+    ];
+    setShips(initialShips);
+  }, []);
       
 
     return (
@@ -76,15 +94,3 @@ export default function Home({ initialShips }) {
         </>
     );
 }
-
-// export async function getServerSideProps() {
-//     // Function to fetch initial data for server-side rendering
-//     const response = await axios.get('https://data.aishub.net/ws.php?username=USERNAME&format=1&output=json&compress=3&latmin=20.5&latmax=30.8&lonmin=-15&lonmax=18.6');
-//     const initialShips = response.data;
-  
-//     return {
-//       props: {
-//         initialShips,
-//       },
-//     };
-//   }
--- components/Map/Map.js
+++ components/Map/Map.js
@@ -1,68 +1,135 @@
+// import { useEffect, useState } from 'react';
+// import L from 'leaflet';
+// import 'leaflet/dist/leaflet.css';
+
+// const Map = ({ ships, addNewShip, deleteShip, updateShip }) => {
+
+//   const [selectedShip, setSelectedShip] = useState(null);
+
+
+//   useEffect(() => {
+//     const map = L.map('map').setView([0, 0], 2);
+//     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+//     ships.forEach((ship) => {
+//         addMarkerToMap(map, ship);
+//       });
+  
+//       // Cleanup map when component unmounts
+//       return () => map.remove();
+//     }, [ships]);
+  
+//     const addMarkerToMap = (map, ship) => {
+//       const marker = L.marker([ship.lat, ship.lng])
+      
+//         .addTo(map)
+//         .bindPopup(`<b>${ship.name}</b><br>ID: ${ship.id}<br>Latitude: ${ship.lat}<br>Longitude: ${ship.lng}`)
+//         .on('contextmenu', function (e) {
+//           e.originalEvent.preventDefault();
+//           const confirmDelete = window.confirm(`Do you want to delete ${ship.name}?`);
+//           if (confirmDelete) {
+//             deleteShip(ship.id);
+//           }
+//         })
+//         .on('click', function () {
+//           setSelectedShip(ship);
+//         });
+//     };
+
+
+//     const handleUpdate = () => {
+//       const newName = prompt('Enter new name:');
+//       const newLat = parseFloat(prompt('Enter new latitude:'));
+//       const newLng = parseFloat(prompt('Enter new longitude:'));
+  
+//       if (newName !== null && !isNaN(newLat) && !isNaN(newLng)) {
+//         updateShip(selectedShip.id, { ...selectedShip, name: newName, lat: newLat, lng: newLng });
+//         setSelectedShip(null);
+//       } else {
+//         alert('Invalid input. Please try again.');
+//       }
+//     };
+
+//   return (
+//     <div>
+//       <div id="map" style={{ height: '500px', maxWidth: '100%' }} />;
+//       {selectedShip && (
+//         <div>
+//           <h3>Update Ship</h3>
+//           <p>ID: {selectedShip.id}</p>
+//           <p>Name: {selectedShip.name}</p>
+//           <p>Latitude: {selectedShip.lat}</p>
+//           <p>Longitude: {selectedShip.lng}</p>
+//           <button onClick={handleUpdate}>Update</button>
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default Map;
+
 import { useEffect, useState } from 'react';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import WebSocket from 'react-websocket';
 
 const Map = ({ ships, addNewShip, deleteShip, updateShip }) => {
+  const [aisData, setAisData] = useState([]);
+  // const apiKey = '763d40ccec7d458e5e4d34dc52e2e5f657a92b36';
 
-  const [selectedShip, setSelectedShip] = useState(null);
-
+  const handleAisData = (data) => {
+    const aisMessage = JSON.parse(data);
+    setAisData((prevData) => [...prevData, aisMessage]);
+  };
 
   useEffect(() => {
-    const map = L.map('map').setView([0, 0], 2);
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+    aisData.forEach((aisMessage) => {
+      updateShip(aisMessage.id, { ...aisMessage, lat: aisMessage.latitude, lng: aisMessage.longitude });
+    });
+  }, [aisData]);
 
-    ships.forEach((ship) => {
-        addMarkerToMap(map, ship);
-      });
-  
-      // Cleanup map when component unmounts
-      return () => map.remove();
-    }, [ships]);
-  
-    const addMarkerToMap = (map, ship) => {
-      const marker = L.marker([ship.lat, ship.lng])
-      
-        .addTo(map)
-        .bindPopup(`<b>${ship.name}</b><br>ID: ${ship.id}<br>Latitude: ${ship.lat}<br>Longitude: ${ship.lng}`)
-        .on('contextmenu', function (e) {
-          e.originalEvent.preventDefault();
-          const confirmDelete = window.confirm(`Do you want to delete ${ship.name}?`);
-          if (confirmDelete) {
-            deleteShip(ship.id);
-          }
-        })
-        .on('click', function () {
-          setSelectedShip(ship);
-        });
-    };
-
-
-    const handleUpdate = () => {
-      const newName = prompt('Enter new name:');
-      const newLat = parseFloat(prompt('Enter new latitude:'));
-      const newLng = parseFloat(prompt('Enter new longitude:'));
-  
-      if (newName !== null && !isNaN(newLat) && !isNaN(newLng)) {
-        updateShip(selectedShip.id, { ...selectedShip, name: newName, lat: newLat, lng: newLng });
-        setSelectedShip(null);
-      } else {
-        alert('Invalid input. Please try again.');
-      }
-    };
+  const openSocket = () => {
+    const socket = new WebSocket('wss://stream.aisstream.io/v0/stream');
+    socket.addEventListener('open', (_) => {
+      const subscriptionMessage = {
+        APIkey: "763d40ccec7d458e5e4d34dc52e2e5f657a92b36",
+        BoundingBoxes: [
+          [
+            [-180, -90],
+            [180, 90],
+          ],
+        ],
+      };
+      socket.send(JSON.stringify(subscriptionMessage));
+    });
+    return socket;
+  };
 
   return (
     <div>
-      <div id="map" style={{ height: '500px', maxWidth: '100%' }} />;
-      {selectedShip && (
-        <div>
-          <h3>Update Ship</h3>
-          <p>ID: {selectedShip.id}</p>
-          <p>Name: {selectedShip.name}</p>
-          <p>Latitude: {selectedShip.lat}</p>
-          <p>Longitude: {selectedShip.lng}</p>
-          <button onClick={handleUpdate}>Update</button>
-        </div>
-      )}
+      <MapContainer center={[0, 0]} zoom={2} style={{ height: '500px', maxWidth: '100%' }}>
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        {ships.map((ship) => (
+          <Marker key={ship.id} position={[ship.lat, ship.lng]}>
+            <Popup>
+              <b>{ship.name}</b>
+              <br />
+              ID: {ship.id}
+              <br />
+              Latitude: {ship.lat}
+              <br />
+              Longitude: {ship.lng}
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
+      <WebSocket
+        url={openSocket}
+        onMessage={handleAisData}
+        reconnect={true}
+      />
     </div>
   );
 };
